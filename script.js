fb66fd4cdb95e4ea23380d61d700eeb9bdde3d75
GabrielBRQ <gabrielmedvalle@gmail.com>
2023-03-10 18:04:30 -0300

    Add comments

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var squares = 16;
 var rainbow = false;
 var color = 000;
 
+//button to change the number of squares
 gridButton.addEventListener('click', () => {
     squares = prompt('Choose a number between 1 and 64: ', '');
     if(squares > 64 || squares < 1){
@@ -18,7 +19,7 @@ gridButton.addEventListener('click', () => {
         alert('Please type a number');
     }   
 })
-
+//clear screen
 clearButton.addEventListener('click', () => {
     container.innerHTML = ''
     createGrid(squares);
@@ -28,26 +29,25 @@ rainbowButton.addEventListener('click', () => {
     rainbow = true;
 })
 
+//function to adapt squares to screen size
 function calcSquareSize(squares){
-    var size = 350;
-    for(;size * squares > 350; size = size - 1){}
+    var size = 400;
+    for(;size * squares > 400; size = size - 0.1){}
     container.style.setProperty('--size', size + 'px');
     container.style.setProperty('--size', size + 'px'); 
-    document.getElementById("container").style.gridTemplateColumns = "repeat(var(--grid-cols), var(--size))";
-    document.getElementById("container").style.gridTemplateRows = "repeat(var(--grid-cols), var(--size))";
 }
 
+//create and 'paint' squares
 function createGrid(cols){
     container.style.setProperty('--grid-cols', cols);
     container.style.setProperty('--grid-rows', cols); 
-
-    
     
     let squareSize = document.getElementById('container').clientWidth / cols;
     for(x = 1; x <= (cols * cols); x++) {
         const color = Math.floor(Math.random()*16777215).toString(16);   
         const board = document.createElement('div');
         board.className = "board";
+        
         container.appendChild(board);
         board.addEventListener('mouseover', () => {
             if(rainbow){
