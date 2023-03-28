d6f6dcbcbcea79317f1dd543366a77474cca4d12
GabrielBRQ <gabrielmedvalle@gmail.com>
2023-03-28 18:02:00 -0300

    format document

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ var color = 000;
 //button to change the number of squares
 gridButton.addEventListener('click', () => {
     let answer = prompt('Choose a number between 1 and 64: ', '');
-    if(answer > 64 || answer < 1){
-        alert('We only accept numbers between 1 and 64');     
-    }else if (answer < 65 && answer> 0){
+    if (answer > 64 || answer < 1) {
+        alert('We only accept numbers between 1 and 64');
+    } else if (answer < 65 && answer > 0) {
         squares = +answer;
-        calcSquareSize(squares);       
+        calcSquareSize(squares);
         container.innerHTML = ''
         createGrid(squares);
-    }else{
+    } else {
         alert('Please type a number');
     }
-    deactivateEraser(); 
+    deactivateEraser();
 });
 
 //clear screen
@@ -32,10 +32,10 @@ clearButton.addEventListener('click', () => {
 });
 
 rainbowButton.addEventListener('click', () => {
-    if(rainbow){
+    if (rainbow) {
         rainbow = false;
         rainbowButton.style.backgroundColor = 'aliceblue';
-    }else{
+    } else {
         rainbowButton.style.backgroundColor = '#384558';
         rainbow = true;
         deactivateEraser();
@@ -43,57 +43,57 @@ rainbowButton.addEventListener('click', () => {
 });
 
 eraserButton.addEventListener('click', () => {
-    if(eraser){
+    if (eraser) {
         deactivateEraser();
-    }else{
+    } else {
         eraserButton.style.backgroundColor = '#384558';
-        eraser= true;
-    }  
+        eraser = true;
+    }
 });
 
-function deactivateEraser(){
+function deactivateEraser() {
     eraser = false;
     eraserButton.style.backgroundColor = 'aliceblue';
 }
 
 //function to adapt squares to screen size
-function calcSquareSize(squares){
-    if(window.innerWidth < 1000){
-        var size = ((window.innerWidth/squares) / 1.8);
-    }else{
-        var size = ((window.innerWidth/squares) / 3);
+function calcSquareSize(squares) {
+    if (window.innerWidth < 1000) {
+        var size = ((window.innerWidth / squares) / 1.8);
+    } else {
+        var size = ((window.innerWidth / squares) / 3);
     }
-    
-    for(;size > window.innerWidth; size = size - 0.1){}
+
+    for (; size > window.innerWidth; size = size - 0.1) { }
     container.style.setProperty('--size', size + 'px');
 };
 
 //create and 'paint' squares
-function createGrid(cols){
+function createGrid(cols) {
     calcSquareSize(cols);
-     
+
     container.style.setProperty('--grid-cols', cols);
-    container.style.setProperty('--grid-rows', cols); 
-    
+    container.style.setProperty('--grid-rows', cols);
+
     let squareSize = document.getElementById('container').clientWidth / cols;
-    for(x = 1; x <= (cols * cols); x++) {
-        const color = Math.floor(Math.random()*16777215).toString(16);   
+    for (x = 1; x <= (cols * cols); x++) {
+        const color = Math.floor(Math.random() * 16777215).toString(16);
         const board = document.createElement('div');
         board.className = "board";
-        
+
         container.appendChild(board);
         board.addEventListener('mouseover', () => {
-            if(eraser){
+            if (eraser) {
                 board.style.backgroundColor = 'aliceblue';
             }
-            else if(rainbow){
+            else if (rainbow) {
                 board.style.backgroundColor = "#" + color;
-            }  
-            else{
+            }
+            else {
                 board.style.backgroundColor = 'black';
             }
         })
     }
 }
 
-createGrid (16);
+createGrid(16);
